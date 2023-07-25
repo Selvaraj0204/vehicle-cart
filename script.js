e51c0e348fe1da1dc64c2f73dcc238fb426c6d6d
Selvaraj K <112171173+Selvaraj0204@users.noreply.github.com>
2023-07-25 14:26:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+cwhite = () => document.getElementById("img").setAttribute("src", "https://imgd.aeplcdn.com/370x208/n/nrkmg4a_1519693.jpg?q=75");
+cblack = () => document.getElementById("img").setAttribute("src", "https://imgd.aeplcdn.com/370x208/n/ds8mg4a_1519691.jpg?q=75");
+cblue = () => document.getElementById("img").setAttribute("src", "https://imgd.aeplcdn.com/370x208/n/r3amg4a_1519681.jpg?q=75");
+function cart(){
+    document.getElementById("cartbutton").innerHTML="Added to cart successfully...";
+}
